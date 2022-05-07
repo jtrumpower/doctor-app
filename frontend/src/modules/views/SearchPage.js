c6eeca0fa93e500e637513a192b4989c3c3743a1
Josiah <josiah.trumpower@cengage.com>
2022-05-07 14:41:14 -0400

    added submit button

--- frontend/src/modules/views/SearchPage.js
+++ frontend/src/modules/views/SearchPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Button,
   FormControl,
   FormHelperText,
   Grid,
@@ -9,18 +10,22 @@ import {
   Typography
 } from '@mui/material';
 import Box from '@mui/material/Box';
+import {DataGrid} from '@mui/x-data-grid';
 
 const SearchPage = () => {
 
 
   return (
-      <Stack component="form" marginTop={2}>
+      <Stack component="form" marginTop={3}>
         <Grid container alignItems="flex-end" spacing={3}>
           <Grid item>
             Enter the name of a doctor:
           </Grid>
           <Grid item>
-            <Input id="Name" type="text" />
+            <Input id="Name" type="text" sx={{ width: 200 }} />
+          </Grid>
+          <Grid item>
+            <Button type="submit" variant="outlined">Search</Button>
           </Grid>
         </Grid>
       </Stack>
